72a22c4e3a21ef916ef64898b8d8181411eb3e8a
kastlo92 <castellanimichele92@gmail.com>
2026-04-23 17:24:54 +0200

    added SeparationRayShape2D for player for slide logic

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -3,16 +3,23 @@ extends CharacterBody2D
 
 const SPEED = 200.0
 const JUMP_VELOCITY = -400.0
+@onready var ray_cast_down = $RayCastDown
+var is_jumping = false
 
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
+	else :
+		is_jumping = false
 
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+		is_jumping = true
+
+	
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
@@ -23,11 +30,14 @@ func _physics_process(delta: float) -> void:
 		flip_sprite(get_node("PlayerSprite"), direction)
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-	#if isOnSlope and DownButton is pressed then 
-	#	get the direction the player is facing and make hi mmove
+
 	if Input.is_action_just_pressed("move_down"):
-		velocity.x = direction * SPEED
-	
+		floor_stop_on_slope = false
+		var down_direction = Input.get_action_strength("move_down")
+		velocity.x = down_direction * SPEED
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+	if Input.is_action_just_released("move_down"):
+		floor_stop_on_slope = true
 	
 	move_and_slide()
 	
